29913ce16edd68a7656a6aba0c06f18410d71c3e
Emmanuel Ani <mailsforanih@gmail.com>
2024-04-07 21:08:21 +0100

    change branch for the semantic release

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hello worldsd');
+// console.log('Hello worldsd');
+const moment = require('moment');
+require('moment-timezone');
+
+function convertDateToTimeZone(dateString, timeZone) {
+	if (!timeZone) {
+		return dateString;
+	}
+
+	if (!dateString) {
+		return null;
+	}
+
+	// const date = moment.utc(dateString).tz(timeZone);
+	return moment.tz(dateString, timeZone).format('DD MMM YYYY, HH:MM:SS A');
+}
+
+console.log(convertDateToTimeZone('2024-04-01T01:00:00Z', 'America/Los_Angeles'));
--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 const config = {
-	branches: ['main'],
+	branches: ['testing_ci'],
 	plugins: [
 		'@semantic-release/commit-analyzer',
 		'@semantic-release/release-notes-generator',
