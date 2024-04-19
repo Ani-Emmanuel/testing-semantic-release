2dda5a1d4f112f63770ae4cf33ed8923e13c5703
Emmanuel Ani <mailsforanih@gmail.com>
2024-04-19 18:33:35 +0100

    feat: this a breaking change

--- src/index.js
+++ src/index.js
@@ -16,3 +16,4 @@ function convertDateToTimeZone(dateString, timeZone) {
 console.log(convertDateToTimeZone('2024-04-01T01:00:00Z', 'America/Los_Angeles'));
 console.log('Assume that this is a breaking changes');
 console.log('Assume this is a patch fix');
+console.log('Assume this a breaking fix');
